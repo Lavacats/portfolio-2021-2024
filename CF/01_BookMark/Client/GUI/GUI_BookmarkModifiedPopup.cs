c77fb9786410c8c810e93a23f34264f6674d9df0
Lavacats <synii1212@gmail.com>
2023-11-21 17:29:19 +0900

    Update GUI_BookmarkModifiedPopup.cs

--- CF/01_BookMark/Client/GUI/GUI_BookmarkModifiedPopup.cs
+++ CF/01_BookMark/Client/GUI/GUI_BookmarkModifiedPopup.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using NETWORK.GAME;
-using UnityEngine;
-
 public class GUI_BookmarkModifiedPopup : PopupDisplay
 {
     public class PARAM : IGUIOnOpenedParam, IGUIOnRefreshedParam
