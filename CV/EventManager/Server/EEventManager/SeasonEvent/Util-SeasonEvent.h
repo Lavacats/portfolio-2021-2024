a4cee86b6033e8a035994dec7192fc527aa80b5f
Lavacats <synii1212@gmail.com>
2023-11-21 17:05:33 +0900

    Update and rename CV/EventManager/Server/EEventManager/Util-SeasonEvent.h to CV/EventManager/Server/EEventManager/SeasonEvent/Util-SeasonEvent.h

--- CV/EventManager/Server/EEventManager/SeasonEvent/Util-SeasonEvent.h
+++ CV/EventManager/Server/EEventManager/SeasonEvent/Util-SeasonEvent.h
@@ -2,21 +2,21 @@
 
 namespace Util::SeasonEvent
 {
-	// ½ÃÁğÀÌº¥Æ® DB ·Îµå
+	// ì‹œì¦Œì´ë²¤íŠ¸ DB ë¡œë“œ
 	void SetData_Season_Event(const TLDB::CQuery::SharedPtr& query);  
 
-	// ½ÃÁğÀÌº¥Æ® DB ¾÷µ¥ÀÌÆ®
+	// ì‹œì¦Œì´ë²¤íŠ¸ DB ì—…ë°ì´íŠ¸
 	void SendToDB_Season_EVENT_USER_INFO_Update(const INT64 userID, INT32 curSeason, INT32	seasonEventKind,
 		INT32 seasonMEventMission_1_Kind, INT32 seasonMEventMission_1_Value, 
 		INT32 seasonMEventMission_2_Kind, INT32 seasonMEventMission_2_Value, 
 		INT32 seasonMEventMission_3_Kind, INT32 seasonMEventMission_3_Value,
 		INT32 completeMission, INT64 rewardTime, const QueryPackLinker::SharedPtr& load_balancer = nullptr);
 
-	// ½ÃÁğÀÌº¥Æ® ¼­¹öÀÌµ¿ ´ëÀÀ
+	// ì‹œì¦Œì´ë²¤íŠ¸ ì„œë²„ì´ë™ ëŒ€ì‘
 	void Export_SeasonEvent(const UserSharedPtr& game_user, const EnumUserContents& content, const Int32& server_id_transfer, const bool& is_reset, Actin::ByteBuffer& byte_buffer);
 	void Import_SeasonEvent(const UserSharedPtr& game_user, const EnumUserContents& content, const bool& is_reset, const Int64& time_offset, Actin::ByteBuffer& byte_buffer, const QueryPackLinker::SharedPtr& load_balancer);
 
-	// ½ÃÁğÀÌº¥Æ® ¼­¹öÀÌµ¿ »èÁ¦ ´ëÀÀ
+	// ì‹œì¦Œì´ë²¤íŠ¸ ì„œë²„ì´ë™ ì‚­ì œ ëŒ€ì‘
 	void Delete_UserSeasonEventData(INT64 userID);
 }
 
